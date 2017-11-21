c6facc4d50f10025707a32c1befa6dc99d4aede8
Brooke <Duckiebeewarren@gmail.com>
2017-11-21 13:39:43 -0500

    bug still with link

--- controllers/scraperController.js
+++ controllers/scraperController.js
@@ -1,33 +1,28 @@
 var express = require("express");
-
 var router = express.Router();
-
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
 var request = require("request");
 
-var cheerio = require("cheerio");
 
-var mongoose = require("mongoose");
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
-var notes = require("../models/notes.js");
-var article = require("../models/article.js");
+var Note = require("../models/notes.js");
+var Article = require("../models/article.js");
 
 router.get("/", function(req, res) {
   res.render("index");
 });
 
-// This will get the articles scraped and saved in db and show them in list.
 router.get("/savedarticles", function(req, res) {
 
-  // Grab every doc in the Articles array
+  
   Article.find({}, function(error, doc) {
-    // Log any errors
+
     if (error) {
       console.log(error);
     }
-    // Or send the doc to the browser as a json object
     else {
       var hbsArticleObject = {
         articles: doc
@@ -38,26 +33,21 @@ router.get("/savedarticles", function(req, res) {
   });
 });
 
-// A GET request to scrape the echojs website
 router.post("/scrape", function(req, res) {
 
-  // First, we grab the body of the html with request
-  request("http://www.nytimes.com/", function(error, response, html) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
+
+  request("http://www.the-leaky-cauldron.org/", function(error, response, html) {
     var $ = cheerio.load(html);
 
-    // Make emptry array for temporarily saving and showing scraped Articles.
+  
     var scrapedArticles = {};
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("#graywell .post").each(function(i, element) {
 
-      // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this).children("a").text();
 
-      console.log("What's the result title? " + result.title);
+      console.log(result.title);
       
       result.link = $(this).children("a").attr("href");
 
@@ -65,7 +55,7 @@ router.post("/scrape", function(req, res) {
 
     });
 
-    console.log("Scraped Articles object built nicely: " + scrapedArticles);
+    console.log(scrapedArticles);
 
     var hbsArticleObject = {
         articles: scrapedArticles
@@ -78,7 +68,7 @@ router.post("/scrape", function(req, res) {
 
 router.post("/save", function(req, res) {
 
-  console.log("This is the title: " + req.body.title);
+  console.log("Title: " + req.body.title);
 
   var newArticleObject = {};
 
@@ -88,15 +78,15 @@ router.post("/save", function(req, res) {
 
   var entry = new Article(newArticleObject);
 
-  console.log("We can save the article: " + entry);
+  console.log(entry);
 
-  // Now, save that entry to the db
   entry.save(function(err, doc) {
-    // Log any errors
+   
     if (err) {
+      console.log("Unable to save article:");
       console.log(err);
     }
-    // Or log the doc
+   
     else {
       console.log(doc);
     }
@@ -108,70 +98,61 @@ router.post("/save", function(req, res) {
 
 router.get("/delete/:id", function(req, res) {
 
-  console.log("ID is getting read for delete" + req.params.id);
-
-  console.log("Able to activate delete function.");
+  console.log(req.params.id);
 
   Article.findOneAndRemove({"_id": req.params.id}, function (err, offer) {
     if (err) {
-      console.log("Not able to delete:" + err);
+      console.log("Not able to delete:");
+      console.log(err);
     } else {
-      console.log("Able to delete, Yay");
+          res.redirect("/savedarticles");
     }
-    res.redirect("/savedarticles");
   });
 });
 
 router.get("/notes/:id", function(req, res) {
 
-  console.log("ID is getting read for delete" + req.params.id);
-
-  console.log("Able to activate delete function.");
+  console.log(req.params.id);
 
   Note.findOneAndRemove({"_id": req.params.id}, function (err, doc) {
     if (err) {
-      console.log("Not able to delete:" + err);
+      console.log("Not able to delete:");
+      console.log(err);
     } else {
-      console.log("Able to delete, Yay");
+          res.send(doc);
     }
-    res.send(doc);
   });
 });
 
-// This will grab an article by it's ObjectId
 router.get("/articles/:id", function(req, res) {
 
   console.log("ID is getting read" + req.params.id);
 
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   Article.findOne({"_id": req.params.id})
 
   .populate('notes')
 
   .exec(function(err, doc) {
     if (err) {
-      console.log("Not able to find article and get notes.");
+      console.log("Not able to retrieve article notes:");
+      console.log(err);
     }
     else {
-      console.log("We are getting article and maybe notes? " + doc);
       res.json(doc);
     }
   });
 });
 
-// Create a new note or replace an existing note
+
 router.post("/articles/:id", function(req, res) {
 
-  // Create a new note and pass the req.body to the entry
   var newNote = new Note(req.body);
-  // And save the new note the db
   newNote.save(function(error, doc) {
-    // Log any errors
     if (error) {
       console.log(error);
     } 
     else {
-      // Use the article id to find it and then push note
+     
       Article.findOneAndUpdate({ "_id": req.params.id }, {$push: {notes: doc._id}}, {new: true, upsert: true})
 
       .populate('notes')
@@ -179,13 +160,14 @@ router.post("/articles/:id", function(req, res) {
       .exec(function (err, doc) {
         if (err) {
           console.log("Cannot find article.");
+          console.log(err);
         } else {
-          console.log("On note save we are getting notes? " + doc.notes);
+          console.log(doc.notes);
           res.send(doc);
         }
       });
     }
   });
 });
-// Export routes for server.js to use.
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,58 +1,50 @@
 
-// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var logger = require("morgan");
+var cheerio = require("cheerio");
 var mongoose = require("mongoose");
-// Requiring our Note and Article models
+var request = require("request");
+var logger = require("morgan");
+
+
 var notes = require("./models/notes.js");
 var article = require("./models/article.js");
-// Our scraping tools
-var request = require("request");
-var cheerio = require("cheerio");
-// Set mongoose to leverage built in JavaScript ES6 Promises
+
 mongoose.Promise = Promise;
 
 var PORT = process.env.PORT || 8080;
 
-// Initialize Express
+
 var app = express();
 
-// Use morgan and body parser with our app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-// Serve static content
 app.use(express.static("public"));
 
-// Set Handlebars.
 var handlebars = require("express-handlebars");
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
 var routes = require("./controllers/scraperController.js");
 
 app.use("/", routes);
 
-mongoose.connect("mongodb://heroku_gnzk5747:4d2121nhgnfbdl1pfirsdepk9n@ds125262.mlab.com:25262/heroku_gnzk5747");
+mongoose.connect("mongodb://heroku_6nfz25c1:sqr3kl19aek12j6env5cteo1ri@ds115446.mlab.com:15446/heroku_6nfz25c1");
 
 var db = mongoose.connection;
 
-// Show any mongoose errors
 db.on("error", function(error) {
   console.log("Mongoose Error: ", error);
 });
 
-// Once logged in to the db through mongoose, log a success message
 db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
-// Listen on port 3000
 app.listen(PORT, function() {
   console.log("App running on PORT " + PORT);
 });
